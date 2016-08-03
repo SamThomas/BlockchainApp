83fecaa72ada02cc2e2e71caf83d3797bccd8e71
Samuel Thomas <samuel.thomas271@gmail.com>
2016-08-03 10:41:28 +0200

    Retrieving Bitcoin exchange rates

---
+++ main.py
@@ -0,0 +1,9 @@
+from blockchain import exchangerates
+
+ticker = exchangerates.get_ticker()
+
+#print the 15 min price for every currency
+for k in ticker:
+    print (k)
+    print (ticker[k].p15min)
+
