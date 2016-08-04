a056bb4a4602b03cc114dbaffc5a974655c5f45c
Samuel Thomas <samuel.thomas271@gmail.com>
2016-08-04 10:00:07 +0200

    Cleaning up & Reorganizing project filetree

---
+++ sources/main.py
@@ -0,0 +1,24 @@
+import sys
+from Tools.scripts.treesync import raw_input
+
+#Blockchian.info libraries
+from blockchain import exchangerates
+from blockchain import wallet
+from blockchain import createwallet
+from blockchain import statistics
+
+ticker = exchangerates.get_ticker()
+
+# Print the 15 min price for every currency
+for k in ticker:
+    print (k, ticker[k].p15min)
+
+#Bitcoin converter
+
+try:
+    amount_to_be_converted = float(raw_input('Amount to be converted in Bitcoins:'))
+except ValueError:
+    print("Not a number")
+
+btc_amount = exchangerates.to_btc('EUR', amount_to_be_converted)
+print(btc_amount)
