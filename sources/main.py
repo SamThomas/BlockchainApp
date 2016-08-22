27f99a3a2e9f812fe03e24c664a6aa344a5f7962
Samuel Thomas <samuel.thomas271@gmail.com>
2016-08-22 13:12:50 +0200

    Removing Blockchain.info code

--- sources/main.py
+++ sources/main.py
@@ -2,36 +2,7 @@ import sys
 from Tools.scripts.treesync import raw_input
 import requests
 import time
-
-# Blockchain.info API
-from blockchain import exchangerates
-
 import matplotlib
-
-ticker = exchangerates.get_ticker()
-
-# Print the 15 min price for every currency
-for k in ticker:
-    print (k, ticker[k].p15min)
-
-# Bitcoin converter
-
-print("*************Welcome to the Bitcoin converter**************")
-
-try:
-    amount_to_be_converted = float(raw_input('Amount to be converted in Bitcoins:'))
-except ValueError:
-    print("Not a number")
-
-try:
-    currency_to_be_converted = input('Currency to be converted in Bitcoins: EUR, USD, GBP, JPY, CAD ')
-except ValueError:
-    print("Not a currency name")
-
-btc_amount = exchangerates.to_btc(currency_to_be_converted, amount_to_be_converted)
-
-print(btc_amount)
-print("************************ END ***********************")
 
 print("++++++++++++++ Bbtcdash ++++++++++++++++")
 
