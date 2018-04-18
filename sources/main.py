2185c9f7c88c6bf8260668b79eb6a0f45710eb2f
SamThomas <samuel.thomas271@gmail.com>
2018-04-18 17:58:11 -0400

    Adding Buy/Sell bitcoin functions from Coinbase API

--- sources/main.py
+++ sources/main.py
@@ -7,10 +7,31 @@ from coinbase.wallet.client import Client
 
 client = Client(api_key="API_KEY", api_secret="API_SECRET", api_version="YYYY-MM-DD");
 
+# Making sure a verified payment method is associated with the Coinbase account.
 #payment_methods = client.get_payment_methods()
 
+# Buy and Sell bitcoins
 
-print("++++++++++++++ Btc toolkit app ++++++++++++++++")
+# account = client.get_primary_account()
+# payment_method = client.get_payment_methods()[0]
+
+buy_price_threshold  = 200
+sell_price_threshold = 500
+
+buy_price  = client.get_buy_price(currency='EUR')
+sell_price = client.get_sell_price(currency='EUR')
+
+
+if float(sell_price.amount) <= sell_price_threshold:
+  sell = account.sell(amount='1',
+                      currency="BTC",
+                      payment_method=payment_method.id)
+
+
+if float(buy_price.amount) <= buy_price_threshold:
+  buy = account.buy(amount='1',
+                    currency="BTC",
+                    payment_method=payment_method.id)
 
 #while 1: # Infinite loop
 for y in range(1):
